04dabd7ab305205a664c327bf71141d46711a9c9
Mohammed-Elsamman <monem7242@gmail.com>
2020-03-20 22:04:48 +0200

    create loginAdmin action

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
+import {GET_ERRORS, SET_CURRENT_USER} from './types';
+
+export const LoginAdmin = (userData) => async (dispatch) => {
+    try {
+        const {data} = await axios.post('http://localhost:4000/admin/', userData);
+        const {token} = data;
+        if (token) {
+            localStorage.setItem('adminToken', token);
+            setAuthToken(token);
+            const decoded = jwt_decode(token);
+            dispatch(setCurrentUser(decoded));
+        } else {
+            dispatch({type: GET_ERRORS, payload: data.errors})
+        }
+    } catch (err) {
+        console.log("no login");
+    }
+};
+
+// Set logged in user
+export const setCurrentUser = decoded => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
+    };
+};
