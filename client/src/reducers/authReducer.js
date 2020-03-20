24ec7ff8a49f6606b2561b457db1f4475a3e8c60
Mohammed-Elsamman <monem7242@gmail.com>
2020-03-20 22:08:30 +0200

    create auth reducer

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,21 @@
+import validation from '../../../validation/inputsValidation';
+
+import { SET_CURRENT_USER } from '../actions/types';
+
+const initialState = {
+  isAuthenticated: false,
+  user: {}
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        isAuthenticated: !validation.isEmpty(action.payload),
+        user: action.payload
+      };
+    default:
+      return state;
+  }
+}
