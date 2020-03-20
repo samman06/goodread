29226a8ad350a319e55a38eef25b6988afcc8d64
Mohammed-Elsamman <monem7242@gmail.com>
2020-03-20 17:37:59 +0200

    create sddNewCstegory function and create route for it

--- controller/CategoryController.js
+++ controller/CategoryController.js
@@ -11,6 +11,20 @@ class CategoryController {
         }
     }
 
+    async addNewCategory(req, res) {
+        const {name} = req.body;
+        if (!req.user.isAdmin) return res.status(400).json({msg: 'Un Authorized Access'});
+        try {
+            let category = await CategoryModel.findOne({name});
+            if (category) return res.json({errors: {name: 'category already exists'}});
+            category = new CategoryModel({name});
+            category = await category.save();
+            return res.json({category})
+        } catch (err) {
+            res.json({err})
+        }
+
+    }
 
 }
 
--- routes/categories.js
+++ routes/categories.js
@@ -1,7 +1,13 @@
 const categoryRouter = require('express').Router();
 const Category = require('../controller/CategoryController');
+const passport = require('passport');
 
 //get all categories
 categoryRouter.get('/', async (req, res) => await Category.getAllCategories(req,res) );
+
+//add new category
+categoryRouter.post('/',
+    passport.authenticate('jwt', {session: false}),
+    (req, res) => Category.addNewCategory(req,res));
 
 module.exports = categoryRouter;
