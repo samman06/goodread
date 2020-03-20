6adf7032bec022ff30841dc7334accb33687c0ce
Mohammed-Elsamman <monem7242@gmail.com>
2020-03-21 00:13:00 +0200

    create editCategoryModal component

--- client/src/components/categoryAdmin/categoryOperations.js
+++ client/src/components/categoryAdmin/categoryOperations.js
@@ -1,34 +1,52 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import React, {Component} from 'react';
+import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
-import { getCategories, addCategory, editCategory, deleteCategory } from "../../actions/categoryActions";
-import CategoryItem from "./CategoryItem";
-import AddCategoryModal from "./addCategoryModal";
+import {getCategories, addCategory, editCategory, deleteCategory} from "../../actions/categoryActions";
+import CategoryItem from "./CategoryItem"
+import AddCategoryModal from "./addCategoryModal"
+import EditCategoryModal from "./editCategoryModal";
 
 class CategoryOperations extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            addModal: false,
+            addModal: false, editModal: false,
             name: "", categoryId: "",
         };
     }
 
     componentDidMount = async () => await this.props.getCategories();
-    onChange = ({ target }) => this.setState({ [target.name]: target.value });
+    onChange = ({target}) => this.setState({[target.name]: target.value});
     openAddModal = () => {
         const addModal = !this.state.addModal;
-        this.setState({ addModal });
+        this.setState({addModal});
+    };
+    parseCategoryData = async (target) => {
+        if (target.name === "edit") {
+            const {name, _id} = await JSON.parse(target.value);
+            this.setState({name, categoryId: _id});
+        }
     };
     addCategory = async () => {
         const {payload} = await this.props.addCategory({name: this.state.name});
         if (payload._id) this.setState({addModal: false,})
     };
+    openEditModal = async (target) => {
+        await this.parseCategoryData(target);
+        const editModal = !this.state.editModal;
+        this.setState({editModal});
+    };
+    editCategory = async () => {
+        const {name, categoryId} = this.state;
+        const {message} = await this.props.editCategory(categoryId, {name});
+        if (message) this.setState({name: "", editModal: false});
+    };
     deleteCategory = async (id) => await this.props.deleteCategory(id);
+
     render() {
-        const { addModal } = this.state;
-        const { categories } = this.props.category;
-        const { errors } = this.props;
+        const {name, addModal, editModal} = this.state;
+        const {categories} = this.props.category;
+        const {errors} = this.props;
         console.log(errors);
         return (
             <div className="col-sm-12">
@@ -38,9 +56,17 @@ class CategoryOperations extends Component {
                     openAddModal={this.openAddModal}
                     addCategory={this.addCategory}
                 />
+                <EditCategoryModal
+                    name={name} errors={errors}
+                    isOpen={editModal}
+                    onChange={this.onChange}
+                    openEditModal={this.openEditModal}
+                    editCategory={this.editCategory}
+                />
                 <CategoryItem
                     categories={categories}
                     deleteCategory={this.deleteCategory}
+                    openEditModal={this.openEditModal}
                 />
             </div>
         );
@@ -55,5 +81,5 @@ CategoryOperations.protoTypes = {
     category: PropTypes.object.isRequired,
     errors: PropTypes.object.isRequired,
 };
-const mapStateToProps = ({ auth, errors, category }) => ({ auth, errors, category });
-export default connect(mapStateToProps, { getCategories, addCategory, editCategory, deleteCategory })(CategoryOperations);
+const mapStateToProps = ({auth, errors, category}) => ({auth, errors, category});
+export default connect(mapStateToProps, {getCategories, addCategory, editCategory, deleteCategory})(CategoryOperations);
---
+++ client/src/components/categoryAdmin/editCategoryModal.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import {FormGroup, Modal, ModalBody, ModalFooter, ModalHeader} from "reactstrap";
+
+class openEditModal extends Component {
+    openEditModal = (event) => this.props.openEditModal(event);
+    onChange = (event) => this.props.onChange(event);
+    editCategory = () => this.props.editCategory();
+
+    render() {
+        const {isOpen, name, errors} = this.props;
+        return (
+            <div>
+                <Modal isOpen={isOpen} toggle={this.openEditModal}>
+                    <ModalHeader toggle={this.openEditModal}>Edit Category</ModalHeader>
+                    <ModalBody>
+                        <FormGroup>
+                            <input
+                                type="name" name="name"
+                                placeholder="Category Name"
+                                value={name}
+                                onChange={this.onChange}
+                            />
+                            {errors.name && <span>{errors.name}</span>}
+                        </FormGroup>
+
+                    </ModalBody>
+                    <ModalFooter>
+                        <button onClick={this.openEditModal && this.editCategory} color="primary">
+                            Edit
+                        </button>
+                        {' '}
+                        <button color="secondary" onClick={this.openEditModal}>Cancel</button>
+                    </ModalFooter>
+                </Modal>
+            </div>
+        );
+    }
+}
+
+export default openEditModal;
