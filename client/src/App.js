f35eb530ca3389a6a56a97f4dc1483d3d46782f0
Mohammed-Elsamman <monem7242@gmail.com>
2020-03-20 21:58:30 +0200

    create redux store and provider and reducers directory

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from './store';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
+        <BrowserRouter>
           <div className="App">
             hello from client
         </div>
+        </BrowserRouter>
+      </Provider>
 
     );
   }
