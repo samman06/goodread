4547e0a746d749a8b4227b207021825e32ed847f
Mohammed-Elsamman <monem7242@gmail.com>
2020-03-20 21:58:13 +0200

    create action types

---
+++ client/src/actions/types.js
@@ -0,0 +1,28 @@
+export const GET_ERRORS = 'GET_ERRORS';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
+export const SET_CURRENT_USER = 'SET_CURRENT_USER';
+
+export const AUTHORS_LOADING = 'AUTHORS_LOADING';
+export const GET_AUTHORS = 'GET_AUTHORS';
+export const GET_AUTHOR = 'GET_AUTHOR';
+export const ADD_AUTHOR = 'ADD_AUTHOR';
+export const DELETE_AUTHOR = 'DELETE_AUTHOR';
+
+export const CATEGORIES_LOADING = 'CATEGORIES_LOADING';
+export const GET_CATEGORIES = 'GET_CATEGORIES';
+export const GET_CATEGORY_BOOKS = 'GET_CATEGORY_BOOKS';
+export const GET_CATEGORY = 'GET_CATEGORY';
+export const ADD_CATEGORY = 'ADD_CATEGORY ';
+export const DELETE_CATEGORY = 'DELETE_CATEGORY ';
+
+export const BOOKS_LOADING = 'BOOKS_LOADING';
+export const GET_BOOKS = 'GET_BOOKS';
+export const GET_BOOK = 'GET_BOOK';
+export const DELETE_BOOK = 'DELETE_BOOK';
+export const ADD_BOOK = 'ADD_BOOK ';
+
+export const GET_USER_BOOKS = 'GET_USER_BOOKS';
+export const GET_USER_BOOKS_STATUS = 'GET_USER_BOOKS_STATUS';
+export const GET_READ_BOOKS = 'GET_READ_BOOKS';
+export const GET_WILL_READ_BOOKS = 'GET_WILL_READ_BOOKS';
+export const GET_READING_BOOKS = 'GET_READING_BOOKS';
