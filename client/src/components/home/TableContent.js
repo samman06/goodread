b1c5e6d3549f8fedcdd5adb4bbfa2734df0775f2
Mohammed-Elsamman <monem7242@gmail.com>
2020-03-21 03:30:11 +0200

    render all userBooks in TableContent component

--- client/src/components/home/TableContent.js
+++ client/src/components/home/TableContent.js
@@ -1,7 +1,12 @@
 import React, {Component} from 'react';
 import {connect} from "react-redux";
 import PropTypes from "prop-types"
+import {Progress} from 'reactstrap';
 import SideBar from "./SideBar";
+import {Link} from "react-router-dom";
+
+import {getUserBooksStatus, getUserBooks, setReadingStatus} from "../../actions/userBooksActions";
+
 
 class TableContent extends Component {
     constructor(props) {
@@ -9,14 +14,43 @@ class TableContent extends Component {
         this.userId = this.props.auth.user._id;
     }
 
-
-    all = async () => console.log("all");
+    componentDidMount = async () => await this.props.getUserBooks(this.userId);
+    all = async () => await this.props.getUserBooks(this.userId);
     read = async () => console.log("read");
     currentlyRead = async () => console.log("current");
     wantToRead = async () => console.log("willRead");
 
 
     render() {
+        const {books} = this.props.userBooks;
+        let currentBooks;
+        if (books) {
+            currentBooks = books.map(({shelve, _id, bookId, rate}, index) =>
+                <tr key={_id}>
+                    <th scope="row">{index}</th>
+                    <td>
+                        <img src={`http://localhost:4000/${bookId.photo}`}
+                             width="50" height="50" alt="error image"/>
+                    </td>
+                    <td>
+                        <Link to={`/books/${bookId._id}`}>{bookId.name}</Link>
+                    </td>
+                    <td>
+                        <Link to={`/authors/${bookId.authorId._id}`}>
+                            {bookId.authorId.firstName + " " + bookId.authorId.lastName}
+                        </Link>
+
+                    </td>
+                    <td>
+                        <Progress value={rate}/>
+                    </td>
+                    <td>
+                        <select className="form-control" name="status">
+                        </select>
+                    </td>
+                </tr>
+            )
+        }
         return (
             <div className="container-fluid">
                 <div className="row">
@@ -40,6 +74,7 @@ class TableContent extends Component {
                             </tr>
                             </thead>
                             <tbody>
+                            {currentBooks}
                             </tbody>
                         </table>
                     </div>
@@ -57,4 +92,4 @@ TableContent.protoTypes = {
 };
 
 const mapStateToProps = ({auth, userBooks}) => ({auth, userBooks});
-export default connect(mapStateToProps, {})(TableContent)
+export default connect(mapStateToProps, {getUserBooksStatus, getUserBooks, setReadingStatus})(TableContent)
