630bd64380d24b0536449cd5c232c0506cd6cd95
Mohammed-Elsamman <monem7242@gmail.com>
2020-03-21 02:02:52 +0200

    create addBookModal component

---
+++ client/src/components/bookAdmin/addBookModal.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react';
+import {Modal, ModalHeader} from "reactstrap";
+
+class addBookModal extends Component {
+    addBookModal = () => this.props.addBookModal();
+    onChange = ({target}) => this.props.onChange(target);
+    addBook = () => this.props.addBook();
+
+    render() {
+        const {authors, categories, isOpen, errors} = this.props;
+        let allCategories, allAuthors;
+        if (authors)
+            allAuthors = authors.map(({_id, firstName, lastName}) =>
+                <option value={_id} key={_id}>{firstName + " " + lastName}</option>);
+        if (categories)
+            allCategories = categories.map(({_id, name}) => <option key={_id} value={_id}>{name}</option>);
+        return (
+            <div>
+                <button
+                    onClick={this.addBookModal} className='btn btn-info offset-10 mt-1 mb-1'>
+                    Add Book +
+                </button>
+                <Modal isOpen={isOpen} toggle={this.addBookModal}>
+                    <ModalHeader toggle={this.addBookModal}>Add Book</ModalHeader>
+                    <div className="modal-body">
+                        <div className="form-group">
+                            <input className="form-control" name="name" id="name"
+                                   placeholder="Book name" onChange={this.onChange}/>
+                            {errors.name && <span>{errors.name}</span>}
+                        </div>
+                        <div className="form-group">
+                            <select className="form-control" name="categoryId" onChange={this.onChange}>
+                                <option value="0">Select Book</option>
+                                {allCategories}
+                            </select>
+                            {errors.categoryId && <span>{errors.categoryId}</span>}
+                        </div>
+                        <div className="form-group">
+                            <select className="form-control" name="authorId" onChange={this.onChange}>
+                                <option value="0">Select Author</option>
+                                {allAuthors}
+                            </select>
+                            {errors.authorId && <span>{errors.authorId}</span>}
+                        </div>
+                        <div className="form-group">
+                            <label>Upload Image</label>
+                            <input type="file" name="file" onChange={this.onChange}/>
+                        </div>
+                    </div>
+                    <div className="modal-footer">
+                        <button className="btn-primary mr-1" onClick={this.addBookModal && this.addBook}>
+                            Add Book
+                        </button>
+                        <button className="btn-secondary" onClick={this.addBookModal}>
+                            Cancel
+                        </button>
+                    </div>
+                </Modal>
+            </div>
+        );
+    }
+}
+
+export default addBookModal;
--- client/src/components/bookAdmin/bookOperations.js
+++ client/src/components/bookAdmin/bookOperations.js
@@ -2,23 +2,50 @@ import React, {Component} from 'react';
 import {connect} from "react-redux"
 import PropTypes from "prop-types";
 import BookItem from "./bookItem";
+import AddBookModal from "./addBookModal";
 import {getCategories} from "../../actions/categoryActions";
 import {getAuthors} from "../../actions/authorActions";
 import {getBooks, addBook, deleteBook, editBook} from "../../actions/bookActions";
 
 class BookOperations extends Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            editModal: false,
+        };
+    }
     componentDidMount = async () => {
         await this.props.getBooks();
         await this.props.getCategories();
         await this.props.getAuthors();
     };
-
+    onChange = (target) => this.setState({[target.name]: target.value});
+    addBookModal = async () => {
+        const addModal = !this.state.addModal;
+        this.setState({addModal})
+    };
+    addBook = async () => {
+        const {name, categoryId, authorId, photo} = this.state;
+        console.log(categoryId);
+        const book = await this.props.addBook({name, categoryId, authorId, photo});
+        if (book) this.setState({name: "", photo: "", addModal: false});
+    };
     render() {
+        const {addModal} = this.state;
         const {books} = this.props.book;
+        const {authors} = this.props.author;
+        const {categories} = this.props.category;
+        const {errors} = this.props;
         return (
             <div className='row'>
                 <div className='col-sm-12'>
+                    <AddBookModal
+                        addBookModal={this.addBookModal}
+                        onChange={this.onChange}
+                        addBook={this.addBook}
+                        errors={errors} categories={categories}
+                        authors={authors} isOpen={addModal}
+                    />
                     <BookItem
                         books={books}
                     />
